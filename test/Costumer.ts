d4b1017e43dcc9b7b1f7a3d4d8cc395b634d1f29
Fiona <fiona.noemi.virnich@hs-furtwangen.de>
2022-02-11 20:26:37 +0100

    neuer test ordner css

---
+++ test/Costumer.ts
@@ -0,0 +1,202 @@
+namespace Doener {
+
+    export class Costumer extends Human {
+
+        myOrder: Storage;
+
+        constructor(_position: number, _x?: number, _y?: number) {
+            super(_position);
+            console.log("Costumer CONSTRUCTOR");
+            this.position = new Vector(_x, _y);
+            this.velocity = new Vector(0, 0);
+            this.velocity.set(100, 0);
+            this.myOrder = this.showOrder();
+            //this.velocity.scale(5);
+        }
+
+        move(_timeslice: number, _x: number, _y: number): void {
+            //console.log("Costumer move");
+            let offset: Vector = new Vector(this.velocity.x, this.velocity.y);
+            offset.scale(_timeslice);
+            this.position.add(offset);
+            crc2.resetTransform;
+            if (this.position.x < 100) {
+                this.position.x += 10;
+                this.velocity.set(10, 0);
+                this.velocity.scale(5);
+
+            }
+
+            if (this.position.x > 830) {
+                this.position.x -= 10;
+                this.velocity.set(-10, 0);
+                this.velocity.scale(5);
+            }
+
+
+        }
+
+        feel(_mood: string) {
+           // console.log("Costumer feel");
+
+            if (_mood == "sad") {
+
+                //console.log("costumer is sad");
+
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.strokeStyle = "black";
+                crc2.beginPath();
+                crc2.ellipse(5, -60, 1, 1, 2, 20, 40);
+                crc2.moveTo(15, -60);
+                crc2.ellipse(15, -60, 1, 1, 2, 20, 40);
+                crc2.moveTo(8, -65);
+                crc2.lineTo(3, -67);
+                crc2.moveTo(12, -65);
+                crc2.lineTo(18, -67);
+                crc2.moveTo(17, -50);
+                crc2.arcTo(7, -55, 5, -50, 5);
+                crc2.closePath();
+                crc2.fill();
+                crc2.stroke();
+            }
+
+            if (_mood == "happy") {
+                // display happy face
+               // console.log("costumer is happy");
+
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.strokeStyle = "black";
+                crc2.beginPath();
+                crc2.ellipse(5, -65, 2, 2, 2, 20, 40);
+                crc2.moveTo(15, -65);
+                crc2.ellipse(15, -65, 2, 2, 2, 20, 40);
+                crc2.moveTo(17, -55);
+                crc2.arcTo(7, -50, 5, -55, 5);
+                crc2.closePath();
+                crc2.fill();
+                crc2.stroke();
+            }
+        }
+
+        draw(): void {
+            // console.log("Customer drawing");
+            crc2.resetTransform();
+            crc2.save();
+            crc2.translate(this.position.x, this.position.y);
+
+            crc2.fillStyle = "red";
+            crc2.strokeStyle = "black";
+
+
+            //Arm rechts
+            crc2.beginPath();
+            crc2.ellipse(35, -30, 5, 12, 2, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+            //Arm links
+            crc2.beginPath();
+            crc2.ellipse(-15, -30, 5, 12, -2, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+            //Bauch
+            crc2.beginPath();
+            crc2.ellipse(10, -25, 20, 25, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+
+            //Kopf
+            crc2.beginPath();
+            crc2.ellipse(10, -60, 15, 15, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            //Füße -> linker Fuß = Nullpunkt
+            crc2.beginPath();
+            crc2.ellipse(0, 0, 8, 7, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(20, 0, 8, 7, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+            crc2.restore();
+
+            //Gesicht
+            crc2.fillStyle = "black";
+            crc2.beginPath();
+            crc2.ellipse(15, -50, 2, 2, 0, 20, 40);
+            crc2.moveTo(15, -50);
+            crc2.lineTo(15, -45);
+            crc2.lineTo(10, -45);
+            crc2.lineTo(20, -45);
+            crc2.lineTo(15, -45);
+
+            crc2.closePath();
+
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(10, -55, 1, 1, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(20, -55, 1, 1, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+        }
+        
+        showOrder(): Storage {
+            let guestOrder: Storage = {
+                bread: 1,
+                tomato: randomOrder(),
+                lettuce: randomOrder(),
+                onion: randomOrder(),
+                meat: randomOrder(),
+            }
+            return guestOrder;
+        }
+    }
+    
+    function order(): Storage {
+       /*  let guestOrder: Storage = {
+            bread: 1,
+            tomato: randomOrder(),
+            lettuce: randomOrder(),
+            onion: randomOrder(),
+            meat: randomOrder(),
+        }
+        return guestOrder; */
+    }
+    
+    function randomOrder(): number{
+
+        let random = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+        return random;
+    }
+    
+}
+
---
+++ test/Worker.ts
@@ -0,0 +1,250 @@
+namespace Doener {
+
+    export class Worker extends Human {
+
+       // currentOrder: Storage
+
+        constructor(_position: number, _x?: number, _y?: number) {
+            super(_position);
+            // console.log("Worker CONSTRUCTOR");
+            this.position = new Vector(_x, _y);
+            this.x = 100;
+            this.y = 500;
+            this.velocity = new Vector(0, 0);
+            this.velocity.set(500, 0);
+          //  this.currentOrder = this.order();
+
+            //this.velocity.scale(5);
+        }
+
+        move(_timeslice: number, x: number, y: number): void {
+            //  console.log("Worker move");
+            let offset: Vector = new Vector(this.velocity.x, this.velocity.y);
+            offset.scale(_timeslice);
+           // this.position.add(offset);
+
+           if (this.position.x < x) {
+            this.position.x ++;
+
+           }
+
+           if (this.position.x > x) {
+            this.position.x --
+
+           }
+           if (this.position.y < y) {
+            this.position.y ++;
+
+           }
+
+           if (this.position.y > y) {
+            this.position.y --
+
+           }            
+         /*    if (this.position.x < 80) {
+                this.position.x += 10;
+                this.velocity.set(15, 0);
+                this.velocity.scale(5);
+
+            }
+
+            if (this.position.x > 600) {
+                this.position.x -= 10;
+                this.velocity.set(-15, 0);
+                this.velocity.scale(5);
+            } */
+
+
+        }
+
+        feel(_mood: string) {
+            // console.log("Worker feel");
+            if (_mood == "neutral") {
+                // display neutral face
+                //console.log("workers mood is neutral");
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.strokeStyle = "black";
+                crc2.beginPath();
+                crc2.ellipse(5, -65, 2, 2, 2, 20, 40);
+                crc2.moveTo(15, -65);
+                crc2.ellipse(15, -65, 2, 2, 2, 20, 40);
+                crc2.moveTo(17, -55);
+                crc2.lineTo(3, -55);
+                crc2.closePath();
+                crc2.fill();
+                crc2.stroke();
+            }
+
+            if (_mood == "stressed") {
+                // display neutral face
+                // console.log("workers mood is stressed");
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.strokeStyle = "black";
+                crc2.beginPath();
+                crc2.ellipse(5, -60, 1, 1, 2, 20, 40);
+                crc2.moveTo(15, -60);
+                crc2.ellipse(15, -60, 1, 1, 2, 20, 40);
+                crc2.moveTo(8, -65);
+                crc2.lineTo(3, -67);
+                crc2.moveTo(12, -65);
+                crc2.lineTo(18, -67);
+                crc2.moveTo(10, -52);
+                crc2.ellipse(10, -52, 4, 3, 0, 20, 40);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.fill();
+
+            }
+            if (_mood == "tired") {
+                // display neutral face
+                // console.log("workers mood is tired");
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.strokeStyle = "black";
+                crc2.beginPath();
+                crc2.moveTo(12, -60);
+                crc2.lineTo(15, -60);
+                crc2.moveTo(8, -60);
+                crc2.lineTo(5, -60);
+                crc2.moveTo(3, -65);
+                crc2.lineTo(8, -67);
+                crc2.moveTo(18, -65);
+                crc2.lineTo(12, -67);
+                crc2.moveTo(10, -52);
+                crc2.ellipse(10, -52, 1, 2, 0, 20, 40);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.fill();
+
+                crc2.strokeStyle = "blue";
+                crc2.beginPath();
+                crc2.moveTo(20, -75);
+                crc2.lineTo(25, -75);
+                crc2.lineTo(20, -70);
+                crc2.lineTo(25, -70);
+
+                crc2.stroke();
+                crc2.resetTransform();
+                crc2.save();
+                crc2.translate(this.position.x, this.position.y);
+
+                crc2.beginPath();
+                crc2.moveTo(28, -85);
+                crc2.lineTo(33, -85);
+                crc2.lineTo(28, -80);
+                crc2.lineTo(33, -80);
+
+                crc2.stroke();
+
+            }
+
+        }
+
+        draw(): void {
+            // console.log("Worker drawing");
+            crc2.resetTransform();
+            crc2.save();
+            crc2.translate(this.position.x, this.position.y);
+
+            crc2.fillStyle = "yellow";
+
+
+            //Arm rechts
+            crc2.beginPath();
+            crc2.ellipse(35, -30, 5, 12, 2, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+            //Arm links
+            crc2.beginPath();
+            crc2.ellipse(-15, -30, 5, 12, -2, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+            //Bauch
+            crc2.beginPath();
+            crc2.ellipse(10, -25, 20, 25, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+
+            //Kopf
+            crc2.beginPath();
+            crc2.ellipse(10, -60, 15, 15, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            //Füße -> linker Fuß = Nullpunkt
+            crc2.beginPath();
+            crc2.ellipse(0, 0, 8, 7, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(20, 0, 8, 7, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+
+            crc2.restore();
+
+            //Gesicht
+            crc2.fillStyle = "black";
+            crc2.beginPath();
+            crc2.ellipse(15, -50, 2, 2, 0, 20, 40);
+            crc2.moveTo(15, -50);
+            crc2.lineTo(15, -45);
+            crc2.lineTo(10, -45);
+            crc2.lineTo(20, -45);
+            crc2.lineTo(15, -45);
+
+            crc2.closePath();
+
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(10, -55, 1, 1, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.ellipse(20, -55, 1, 1, 0, 20, 40);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+        }
+
+        order(): Storage {
+            let currentOrder: Storage = {
+                bread: 0,
+                tomato: 0,
+                lettuce: 0,
+                onion: 0,
+                meat: 0,
+            }
+            return currentOrder;
+        }
+
+
+
+    }
+}
+
